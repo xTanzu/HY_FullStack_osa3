4f6bec00c5f09751ea210f898b17c9f0aa136c50
Taneli Härkönen <taneliharkonen@hotmail.com>
2023-11-22 04:54:22 +0200

    3.4: puhelinluettelon backend step4
    - make it possible to DELETE a person based on its id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -46,12 +46,17 @@ app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id)
   console.log(`Getting person id: ${id}`)
   person = persons.find(person => person.id === id)
-
   if (person) {
     response.json(person)
   } else {
     response.status(404).send("<p>No person found</p>")
   }
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
