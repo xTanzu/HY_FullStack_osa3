fc7562375c8a03dc77e0f557973050abf340c3af
Taneli Härkönen <taneliharkonen@hotmail.com>
2023-11-22 05:34:38 +0200

    3.5: puhelinluettelon backend step5
    - make it possible to add new persons to the phonebook
      - adding a new person requires to send a "name" and a number to
        the /api/persons using the POST method
      - the new entry will get a a random entry in a numberspace twice
        the size of the length of the phonebook
      - both name and a number is required to add a new person

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
 
 let persons = [
   {
@@ -23,6 +24,14 @@ let persons = [
     number: "39-23-6423122"
   },
 ]
+
+const generateRandomId = (max) => {
+  let id = null
+  do {
+    id = Math.ceil(Math.random() * max)
+  } while (persons.find(person => person.id === id))
+  return id
+}
 
 app.get("/info", (request, response) => {
   console.log("forming info page")
@@ -57,6 +66,26 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
   response.status(204).end()
+})
+
+app.post("/api/persons", (request, response) => {
+  const content = request.body
+
+  if (!content.name) {
+    return response.status(400).json({
+      error: "Both name and number required"
+    })
+  }
+
+  const person = {
+    id: generateRandomId(persons.length * 2),
+    name: content.name,
+    number: content.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
