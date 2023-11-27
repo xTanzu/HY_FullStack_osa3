34a7d2dc7c5b8eba6aa599508d134c2db7f0253d
Taneli Härkönen <taneliharkonen@hotmail.com>
2023-11-27 23:00:08 +0200

    3.12: tietokanta komentoriviltä
    - Make file mongo.js to add people to mongoDB
      - call node to file and give db-password as first argument to list
        people
      - call node to file and give db-password as first argument, name as
        second argument and number as third to add this person to the
        database

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+  console.log("give password as argument")
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://harkonentaneli:${password}@cluster0.pexjyyk.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set("strictQuery", false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model("Person", noteSchema)
+
+if (process.argv.length == 3) {
+  Person.find({}).then(result => {
+    console.log("phonebook:")
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+  })
+  mongoose.connection.close()
+})
+} else if (process.argv.length == 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+  person.save().then(result => {
+    console.log(`new person ${result.name} saved`)
+    mongoose.connection.close()
+  })
+} else {
+  console.log("Not understood, did nothing..")
+  process.exit(1)
+}
